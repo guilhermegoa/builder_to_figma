630799d1f4ec8fb922588fc1a24a15416ae9a1ed
Thadeu Castelo Branco Ramos <thadeucb@hotmail.com>
2024-01-24 17:15:38 -0300

    feat: adding the creation of id element

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { once, showUI } from '@create-figma-plugin/utilities'
 import { type CloseHandler, type CreateFigmaHandler } from './types/eventHandler.type'
 import { getBotJson } from './utils/flowDownloader'
+import { type BlockList } from './utils/jsonReader'
 
 export default function (): void {
   // CREATE_FIGMA event handler
@@ -8,64 +9,83 @@ export default function (): void {
     await figma.loadFontAsync({ family: 'Roboto', style: 'Regular' })
     await figma.loadFontAsync({ family: 'Roboto', style: 'Bold' })
     await figma.loadFontAsync({ family: 'Roboto', style: 'Medium' })
-    console.log('passou')
 
     const json = await getBotJson(key)
     console.log(json)
 
-    json.forEach((block: any) => {
+    async function createElements(block: BlockList): Promise<void> {
       if (block.actions.length > 0) {
         block.actions.forEach(async (action: any) => {
-          switch (action.typeOfContent) {
-            case 'chat-state':
-              console.log('chat-state')
-              break
-            case 'select': {
-              const component = await figma.importComponentByKeyAsync(
-                '8ae2d54f6f76aa2bfe23df1361def19ef9c0249d'
-              )
-              const instance: any = component.createInstance()
-              instance.x = Number(block.position.left.replace('px', ''))
-              instance.y = Number(block.position.top.replace('px', ''))
-              break
+          if (action.content !== false) {
+            switch (action.typeOfContent) {
+              case 'chat-state':
+                break
+              case 'select': {
+                const component = await figma.importComponentByKeyAsync(
+                  '8ae2d54f6f76aa2bfe23df1361def19ef9c0249d'
+                )
+
+                const instance: any = component.createInstance()
+                instance.name = block.id
+                instance.x = Number(block.position.left.replace('px', ''))
+                instance.y = Number(block.position.top.replace('px', ''))
+                break
+              }
+              case 'select-immediate': {
+                const component = await figma.importComponentByKeyAsync(
+                  '38a80132b23e2c09bfdaba75f9e837e2a3d73642'
+                )
+                const instance: any = component.createInstance()
+                instance.name = block.id
+                instance.children[0].children[1].children[0].children[0].children[3].characters =
+                  'Title teste'
+                instance.children[0].children[1].children[0].children[1].children[0].children[0].characters =
+                  action.content // body
+                instance.children[0].children[1].children[2].children[0].children.forEach(
+                  (b: any, idx: any) => {
+                    if (idx < action.options.length) b.children[0].characters = action.options[idx]
+                  }
+                ) // buttons
+                instance.x = Number(block.position.left.replace('px', ''))
+                instance.y = Number(block.position.top.replace('px', ''))
+                break
+              }
+              case 'SendMessage': {
+                const component = await figma.importComponentByKeyAsync(
+                  '2f43d0db522f94bc1a84b8f6a531fcde255679c4'
+                )
+                const instance: any = component.createInstance()
+                instance.name = block.id
+                // eslint-disable-next-line max-len
+                instance.children[0].children[0].children[1].children[0].children[0].children[0].children[0].children[0].characters =
+                  action.content
+                instance.x = Number(block.position.left.replace('px', ''))
+                instance.y = Number(block.position.top.replace('px', ''))
+                break
+              }
+              default:
+                break
             }
-            case 'select-immediate': {
-              const component = await figma.importComponentByKeyAsync(
-                '38a80132b23e2c09bfdaba75f9e837e2a3d73642'
-              )
-              const instance: any = component.createInstance()
-              instance.children[0].children[1].children[0].children[0].children[3].characters = 'Title teste'
-              instance.children[0].children[1].children[0].children[1].children[0].children[0].characters =
-                action.content // body
-              instance.children[0].children[1].children[2].children[0].children.forEach(
-                (b: any, idx: any) => {
-                  if (idx < action.options.length) b.children[0].characters = action.options[idx]
-                }
-              ) // buttons
-              instance.x = Number(block.position.left.replace('px', ''))
-              instance.y = Number(block.position.top.replace('px', ''))
-              break
-            }
-            case 'text':
-            case 'SendMessage': {
-              const component = await figma.importComponentByKeyAsync(
-                '2f43d0db522f94bc1a84b8f6a531fcde255679c4'
-              )
-              const instance: any = component.createInstance()
-              console.log(action)
-              // eslint-disable-next-line max-len
-              instance.children[0].children[0].children[1].children[0].children[0].children[0].children[0].children[0].characters =
-                action.content
-              instance.x = Number(block.position.left.replace('px', ''))
-              instance.y = Number(block.position.top.replace('px', ''))
-              break
-            }
-            default:
-              console.log('default')
-              break
           }
         })
+      } else {
+        const component = await figma.importComponentByKeyAsync('1af1e3e0d5fc69d496d3909c5143edb0f2ace6ea')
+        const instance: any = component.createInstance()
+        instance.name = block.id
+        instance.children[1].characters = block.figmaId
+        instance.x = Number(block.position.left.replace('px', ''))
+        instance.y = Number(block.position.top.replace('px', ''))
       }
+      const idComponent = await figma.importComponentByKeyAsync('6ca6d4651bef9bda556960d72a8427c0f44f25e0')
+      const idInstance: any = idComponent.createInstance()
+      idInstance.x = Number(block.position.left.replace('px', ''))
+      idInstance.y = Number(block.position.top.replace('px', '')) - 30
+      idInstance.name = block.figmaId
+      // idInstance.children[0].characters = 'A.1.2.3' <--- Quebrado
+    }
+
+    json.forEach(async (block: BlockList) => {
+      await createElements(block)
     })
 
     // Update all components NO DELETE
