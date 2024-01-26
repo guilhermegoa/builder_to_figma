53c4b7df2b2740bd55a2903657aca453f04bf48c
Emerson Junqueira <emoonlit@gmail.com>
2024-01-26 10:47:35 -0300

    feat: added reflect id to chieldren node

--- src/instances/id.ts
+++ src/instances/id.ts
@@ -1,10 +1,12 @@
 async function createId(block: any): Promise<InstanceNode> {
   const idComponent = await figma.importComponentByKeyAsync('6ca6d4651bef9bda556960d72a8427c0f44f25e0')
-  const instance: any = idComponent.createInstance()
+  const instance: InstanceNode = idComponent.createInstance()
   instance.x = Number(block.position.left.replace('px', '')) * 2
   instance.y = Number(block.position.top.replace('px', '')) * 2 - 30
   instance.name = block.figmaId
-  // idInstance.children[0].characters = 'A.1.2.3' <--- Quebrado
+
+  const text = instance.children[0] as TextNode
+  text.characters = block.figmaId
 
   return instance
 }
