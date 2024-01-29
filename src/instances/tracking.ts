6339ff49f58947a92c0d421a17418a89d025ba94
Thadeu Castelo Branco Ramos <thadeucb@hotmail.com>
2024-01-29 11:42:12 -0300

    feat: removing the y position of the component tracking

--- src/instances/tracking.ts
+++ src/instances/tracking.ts
@@ -12,7 +12,7 @@ async function createTracking({ tracking, position, index }: ICreateTracking): P
   const idComponent = await figma.importComponentByKeyAsync('0dececd32f95c805215e31e5dedbdbc9bb589e93')
   const instance: InstanceNode = idComponent.createInstance()
   instance.x = Number(position.left.replace('px', '')) * 2 - 230
-  instance.y = Number(position.top.replace('px', '')) * 2 + index * 60
+  instance.y = Number(position.top.replace('px', '')) * 2
   // TODO: futuramente usar o instance.height para fazer calculos dinamicos da posição y
   const title = instance.children[0] as TextNode
   const content = instance.children[1] as TextNode
