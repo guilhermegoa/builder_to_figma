e509504835d49ea43c4e853312a3c39dce5024bc
Thadeu Castelo Branco Ramos <thadeucb@hotmail.com>
2024-01-31 11:42:09 -0300

    feat: creating message handler

---
+++ src/utils/sendMessageHandler.ts
@@ -0,0 +1,42 @@
+import { createSelect, createSelectImediate, createSendMessage, createMacro } from '../instances'
+import { createFigmaGroup } from './figmaGroups'
+import { type BlockList } from './jsonReader'
+
+function sendMessageHandler(components: ComponentNode[], block: BlockList): GroupNode | null {
+  const [selectComponent, selectImediateComponent, sendMessageComponent, macroComponent] = components
+
+  if (block.actions.length > 0) {
+    const components = block.actions
+      .map((action: any): any => {
+        if (action.content !== false) {
+          switch (action.typeOfContent) {
+            case 'chat-state':
+              break
+            case 'select': {
+              return createSelect(selectComponent, block, action)
+            }
+            case 'select-immediate': {
+              return createSelectImediate(selectImediateComponent, block, action)
+            }
+            case 'text':
+            case 'SendMessage': {
+              return createSendMessage(sendMessageComponent, block, action)
+            }
+            default:
+              return createMacro(macroComponent, block)
+          }
+        }
+        return false
+      })
+      .filter((item: any) => item !== false)
+    if (components.length === 0) return null
+    const group = createFigmaGroup(components, 'Message', 'vertical')
+    return group
+  } else {
+    const component = createMacro(macroComponent, block)
+    const group = createFigmaGroup([component], 'Logic Only')
+    return group
+  }
+}
+
+export default sendMessageHandler
