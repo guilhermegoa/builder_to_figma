0aa2b945b726ecc11749a05b005df266496582b7
Guilherme O <guilhergoa@hotmail.com>
2024-01-31 07:57:52 -0300

    feat: doing list message

--- src/instances/select.ts
+++ src/instances/select.ts
@@ -1,8 +1,30 @@
+interface Option extends FrameNode {
+  children: any[] | TextNode[]
+}
+
 function createSelect(component: ComponentNode, block: any): InstanceNode {
   const instance: InstanceNode = component.createInstance()
   instance.name = block.id
   instance.x = Number(block.position.left.replace('px', '')) * 2
   instance.y = Number(block.position.top.replace('px', '')) * 2
+
+  const title = instance.children[0] as TextNode
+  title.characters = 'Select'
+
+  for (let i = 1; i < instance.children.length - 2; i += 2) {
+    const description = instance.children[i] as FrameNode
+    description.visible = false
+
+    const option = instance.children[i + 1] as Option
+    option.visible = true
+
+    const frameOptionTitle = option.children[0].children[0] as Option
+    frameOptionTitle.visible = true
+    frameOptionTitle.children[0].characters = `Option ${i}`
+
+    const frameOptionDescription = option.children[0].children[1] as Option
+    frameOptionDescription.visible = false
+  }
 
   figma.currentPage.appendChild(instance)
   return instance
--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,7 @@ export default function (): void {
       // figma.importComponentByKeyAsync('2f43d0db522f94bc1a84b8f6a531fcde255679c4'),
       figma.importComponentByKeyAsync('1af1e3e0d5fc69d496d3909c5143edb0f2ace6ea'),
       figma.importComponentByKeyAsync('467d5c428b3b2c1b82447d7da01aaa0c3209cba2'),
-      figma.importComponentByKeyAsync('8ae2d54f6f76aa2bfe23df1361def19ef9c0249d'),
+      figma.importComponentByKeyAsync('487efbf0f24c7f25ed162ea14e8389816388d0b1'),
       figma.importComponentByKeyAsync('38a80132b23e2c09bfdaba75f9e837e2a3d73642'),
       figma.importComponentByKeyAsync('2f43d0db522f94bc1a84b8f6a531fcde255679c4'),
       figma.importComponentByKeyAsync('0dececd32f95c805215e31e5dedbdbc9bb589e93')
