bbb709b4b651d3d2b76befdf919ed8758437946c
Thadeu Castelo Branco Ramos <thadeucb@hotmail.com>
2024-01-31 10:54:06 -0300

    feat: updating tracking instance to handle with multiple trackings

--- src/instances/tracking.ts
+++ src/instances/tracking.ts
@@ -1,31 +1,29 @@
-import { type ITracking } from '../utils/jsonReader'
+import { createFigmaGroup } from '../utils/figmaGroups'
+import { type BlockList } from '../utils/jsonReader'
 
-interface ICreateTracking {
-  component: ComponentNode
-  tracking: ITracking
-  position: {
-    left: string
-    top: string
-  }
-  index: number
-}
-function createTracking({ tracking, position, index, component }: ICreateTracking): InstanceNode {
-  const instance: InstanceNode = component.createInstance()
-  instance.x = Number(position.left.replace('px', '')) * 2
-  instance.y = Number(position.top.replace('px', '')) * 2
-  instance.relativeTransform = [
-    [-1, 1.2246468525851679e-16, instance.x],
-    [1.2246468525851679e-16, 1, instance.y]
-  ]
+function createTracking(component: ComponentNode, block: BlockList): GroupNode | null {
+  const instances = block.trackings.map((tracking) => {
+    const instance: InstanceNode = component.createInstance()
 
-  // TODO: futuramente usar o instance.height para fazer calculos dinamicos da posição y
-  const title = instance.children[0] as TextNode
-  const content = instance.children[1] as TextNode
+    instance.x = Number(block.position.left.replace('px', '')) * 2
+    instance.y = Number(block.position.top.replace('px', '')) * 2
+    instance.relativeTransform = [
+      [-1, 1.2246468525851679e-16, instance.x],
+      [1.2246468525851679e-16, 1, instance.y]
+    ]
+    const title = instance.children[0] as TextNode
+    const content = instance.children[1] as TextNode
 
-  title.characters = tracking.category!
-  content.characters = tracking.action!
-  figma.currentPage.appendChild(instance)
-  return instance
+    title.characters = tracking.category!
+    content.characters = tracking.action!
+    figma.currentPage.appendChild(instance)
+
+    return instance
+  })
+
+  const trackingGroup = createFigmaGroup(instances, 'Trackings', 'vertical')
+
+  return trackingGroup
 }
 
 export default createTracking
