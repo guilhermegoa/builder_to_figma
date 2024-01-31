405317b2179f72465b8bccb838af766c4c180b3d
Thadeu Castelo Branco Ramos <thadeucb@hotmail.com>
2024-01-31 11:43:26 -0300

    feat: updating main file

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,10 @@ import { once, showUI } from '@create-figma-plugin/utilities'
 import { type CloseHandler, type CreateFigmaHandler } from './types/eventHandler.type'
 import { getBotJson } from './utils/flowDownloader'
 import { type BlockList } from './utils/jsonReader'
-import {
-  createId,
-  createSelect,
-  createMacro,
-  createSendMessage,
-  createSelectImediate,
-  createTracking,
-  createObservationComponent
-} from './instances/index'
+import { createId, createTracking, createObservationComponent } from './instances/index'
 import components from './utils/figmaComponents'
 import loadFigmaFonts from './utils/loadFigmaFonts'
+import sendMessageHandler from './utils/sendMessageHandler'
 
 export default function (): void {
   // CREATE_FIGMA event handler
@@ -32,39 +25,16 @@ export default function (): void {
 
     const json = await getBotJson(key)
     console.log(json)
-    let lastBlockPosition = { x: 0, y: 0, heigth: 0, width: 0 }
-    async function createElements(block: BlockList): Promise<void> {
-      let instance: any = null
-      if (block.actions.length > 0) {
-        block.actions.forEach(async (action: any) => {
-          if (action.content !== false) {
-            switch (action.typeOfContent) {
-              case 'chat-state':
-                break
-              case 'select': {
-                instance = createSelect(selectComponent, block, action)
-                break
-              }
-              case 'select-immediate': {
-                instance = createSelectImediate(selectImediateComponent, block, action)
-                break
-              }
-              case 'text':
-              case 'SendMessage': {
-                instance = createSendMessage(sendMessageComponent, block, action)
-                break
-              }
-              default:
-                break
-            }
-          }
-        })
-      } else {
-        instance = createMacro(macroComponent, block)
-      }
+
+    function createElements(block: BlockList): void {
+      sendMessageHandler(
+        [selectComponent, selectImediateComponent, sendMessageComponent, macroComponent],
+        block
+      )
 
       createTracking(trackingComponent, block)
-      const idInstance = createId(idComponent, block)
+
+      createId(idComponent, block)
 
       const directionsObservation = block.condicaoSaida
         .map((item) => {
@@ -73,34 +43,30 @@ export default function (): void {
         })
         .join('\n')
 
-      const directionObservationNode = createObservationComponent(
-        observationComponent,
-        block,
-        directionsObservation
-      )
-      if (instance !== null) {
-        const group = figma.group([idInstance, instance, directionObservationNode], figma.currentPage)
+      createObservationComponent(observationComponent, block, directionsObservation)
+      // if (instance !== null) {
+      //   const group = figma.group([idInstance, instance, directionObservationNode], figma.currentPage)
 
-        group.name = block.figmaId
-        // alignGroupVertical(group)
-        const absolutePositionX = lastBlockPosition.x + lastBlockPosition.width
-        const absolutePositionY = lastBlockPosition.y + lastBlockPosition.heigth
-        if (lastBlockPosition.x !== 0) {
-          if (group.x > lastBlockPosition.x && group.x < absolutePositionX) {
-            group.x = absolutePositionX + 10
-          }
-        }
-        if (lastBlockPosition.y !== 0) {
-          if (group.y > lastBlockPosition.y && group.y < absolutePositionY) {
-            group.y = absolutePositionY + 10
-          }
-        }
-        lastBlockPosition = { x: group.x, y: group.y, heigth: group.height, width: group.width }
-      }
+      //   group.name = block.figmaId
+      //   // alignGroupVertical(group)
+      //   const absolutePositionX = lastBlockPosition.x + lastBlockPosition.width
+      //   const absolutePositionY = lastBlockPosition.y + lastBlockPosition.heigth
+      //   if (lastBlockPosition.x !== 0) {
+      //     if (group.x > lastBlockPosition.x && group.x < absolutePositionX) {
+      //       group.x = absolutePositionX + 10
+      //     }
+      //   }
+      //   if (lastBlockPosition.y !== 0) {
+      //     if (group.y > lastBlockPosition.y && group.y < absolutePositionY) {
+      //       group.y = absolutePositionY + 10
+      //     }
+      //   }
+      //   lastBlockPosition = { x: group.x, y: group.y, heigth: group.height, width: group.width }
+      // }
     }
 
     json.forEach(async (block: BlockList) => {
-      await createElements(block)
+      createElements(block)
     })
 
     figma.closePlugin()
